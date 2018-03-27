d56ddba532cf5bc1a0a207e96f255fcf6ef12835
Yaman <Yaman@Chutongs-MacBook-Pro.local>
2018-03-27 16:30:22 -0400

    Initial Commit

---
+++ RestCallVid/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  RestCallVid
+//
+//  Created by Yaman on 3/27/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
